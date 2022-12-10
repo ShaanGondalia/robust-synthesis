265005933fd499100916ae4d3070d1856df92cbd
Shaan Gondalia <shaan.gondalia@duke.edu>
2022-12-10 18:04:23 -0500

    Add basic installation and usage

---
+++ models/README.md
@@ -0,0 +1,2 @@
+# Models
+Add pre-trained robust models downloaded from [here](https://github.com/MadryLab/robustness_applications).
---
+++ user_constants.py
@@ -0,0 +1,6 @@
+# Path to dataset 
+DATA_PATH_DICT = {
+    'CIFAR': '/tmp/',
+    'RestrictedImageNet': '/scratch/engstrom_scratch/imagenet',
+    'ImageNet': '/scratch/engstrom_scratch/imagenet'
+}
